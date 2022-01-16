d8dfaa3879299433be592c77e0526dc9ae40cc44
abgrund10 <72023751+abgrund10@users.noreply.github.com>
2022-01-16 19:18:02 +0300

    Create Circle.py

---
+++ src/Circle.py
@@ -0,0 +1,16 @@
+import math
+
+from src.Figure import Figure
+
+
+class Circle(Figure):
+
+    def __init__(self, r):
+        self.name = "Circle"
+        try:
+            self.perimeter = 2 * math.pi * r
+            self.area = math.pi * r * r
+        except (AttributeError, TypeError) as e:
+            print("Error occured: ", e)
+            self.perimeter = "N/A"
+            self.area = "N/A"
