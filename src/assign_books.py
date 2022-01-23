c3edaf2e08cbb051fa379a6c92a85d300d4e6a9d
tank34 <abgrund1@yandex.ru>
2022-01-23 20:52:17 +0300

    equal distribution works

--- src/assign_books.py
+++ src/assign_books.py
@@ -1,6 +1,9 @@
+import csv
+import itertools
 import urllib.request
 
 import json
+from itertools import islice
 
 urllib.request.urlretrieve("https://raw.githubusercontent.com/konflic/front_example/master/data/books.csv", "books.csv")
 urllib.request.urlretrieve("https://raw.githubusercontent.com/konflic/front_example/master/data/users.json", "users.json")
@@ -9,13 +12,58 @@ with open('users.json', 'r') as users:
     result = []
     json_object = json.loads(users.read())
     for line in json_object:
-        json_data = json.dumps({'name': line['name'], 'gender': line['gender'], 'address': line['address'], 'age': line['age'], 'books': 0 })
+        json_data = json.dumps({'name': line['name'], 'gender': line['gender'], 'address': line['address'], 'age': line['age']})
         result.append(json_data)
-print(result)
 
 amount_of_users = len(result)
 
-with open('books.csv', 'r') as books:
-    json_object = books.read()
-    amount_of_books = len(json_object)
+with open('books.csv') as books:
+    amount_of_books = sum(1 for row in books)
 
+#cdistribute books
+print('amount of books : ' + str(amount_of_books))
+print('amount of users : ' + str(amount_of_users))
+iteration = amount_of_books // amount_of_users
+print('по сколько книг на юзера: ' + str(iteration))
+ostatok = amount_of_books % amount_of_users
+print('остаток от деления: '+ str(ostatok))
+
+#add equal amount of books to users
+if iteration > 0:
+    lower_limit = 0
+    upper_limit = iteration
+    result_updated = []
+    with open('books.csv') as books:
+        csv_reader = csv.reader(books, delimiter=',')
+        books.readline()
+        for user in result:
+            x = json.loads(user)
+            books_array = []
+            for row in itertools.islice(csv_reader, lower_limit, upper_limit):
+                books_array.append(row)
+            x['books'] = books_array
+            result_updated.append(x)
+#print('final array')
+#print(result_updated)
+
+if ostatok > 0:
+    lower_limit = iteration * amount_of_users
+    upper_limit = lower_limit + 1
+    with open('books.csv') as books:
+        csv_reader = csv.reader(books, delimiter=',')
+        books.readline()
+        for user in result_updated:
+            x = json.load(user)
+            print('')
+            print(x)
+            books_array = []
+            books_array.append(x['books'])
+            print('books array')
+            print(books_array)
+            print('')
+            for row in itertools.islice(csv_reader, lower_limit, upper_limit):
+                books_array.append(row)
+            x['books'] = books_array
+            result_updated.append(x)
+print('final array')
+print(result_updated)
