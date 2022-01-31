ddc79be5aa908c7483d62595ce2914d79f49c7d1
abgrund10 <72023751+abgrund10@users.noreply.github.com>
2022-01-31 13:12:27 +0300

    final update

--- src/rest_api_4.py
+++ src/rest_api_4.py
@@ -4,10 +4,4 @@ import requests
 
 def test_url(url, status_code):
     response = requests.get(url)
-
-    try:
-        assert r.status_code == int(status_code)
-
-    except URLError:
-        assert int(status_code) == 404
-        assert response.status_code == 404
+    assert r.status_code == int(status_code)
