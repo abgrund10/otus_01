f050b9b88ae185034272116b1dfa260ac674b7dc
tank34 <abgrund1@yandex.ru>
2022-05-28 15:13:46 +0300

    updated files

--- src/Parsing_files_from_dir.py
+++ src/Parsing_files_from_dir.py
@@ -18,12 +18,19 @@ def main():
     parser = argparse.ArgumentParser(description='Process command line arguments.')
     parser.add_argument('--path')
     args = parser.parse_args()
-    cwd = os.getcwd()
-    new_way = cwd + "/" + args.path
-    new_file = find('*.log', args.path)
-    os.chdir(new_way)
-    for f in new_file:
-        Parsing_file.parsing_file(f)
+    if os.path.isdir(args.path):
+        cwd = os.getcwd()
+        new_way = cwd + "/" + args.path
+        new_file = find('*.log', args.path)
+        os.chdir(new_way)
+        for f in new_file:
+            Parsing_file.parsing_file(f)
+    elif os.path.isfile(args.path):
+        os.chdir(os.path.dirname(args.path))
+        filename = os.path.basename(args.path)
+        Parsing_file.parsing_file(filename)
+    else:
+        raise Exception("Please check path to your file")
 
 
 if __name__ == "__main__":
