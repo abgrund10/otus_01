d94b8a3cee8e3b1082f2134edf7b1dd8ed03cd2e
tank34 <abgrund1@yandex.ru>
2022-06-08 02:43:53 +0300

    failed attemot

---
+++ src/server.py
@@ -0,0 +1,32 @@
+import socket
+import re
+from http import HTTPStatus
+
+HOST = "127.0.0.1"
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    print(f"Server is started on {HOST}:{PORT}")
+    while True:
+        data = conn.recv(1024)
+        string = str(data)
+        value = re.split('status=', string)[1]
+        print(value)
+        method = string.split()[0]
+        for status in HTTPStatus:
+            if value == status:
+                status_value = status.value
+                status_phrase = status.phrase
+                break
+            else:
+                status_value = HTTPStatus.OK
+                status_phrase = HTTPStatus(HTTPStatus.OK).phrase
+
+        response_headers = "Content-Type: text/html; charset=utf-8\n".encode("utf-8")
+        conn.send(response_headers + f"Request Method: {method}"
+                                     f"Request Source: ({HOST},{PORT})"
+                                     f"Response Status: {status_value} {status_phrase}".encode("utf-8"))
+    # conn.close()
