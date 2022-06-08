9204bb7e22a564b26b54c308d049ced71cccc74f
tank34 <abgrund1@yandex.ru>
2022-06-09 00:11:12 +0300

    updated while loop

--- src/client.py
+++ src/client.py
@@ -5,9 +5,9 @@ import socket
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"GET http://127.0.0.1/status=404 NotFound")
-    data = s.recv(1024)
-
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall(b"GET http://127.0.0.1/status=404 NotFound")
+data = s.recv(1024)
+s.close()
 print("Received: " + str(data))
--- src/server.py
+++ src/server.py
@@ -5,14 +5,15 @@ from http import HTTPStatus
 HOST = "127.0.0.1"
 PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    print(f"Server is started on {HOST}:{PORT}")
-    while True:
-        data = conn.recv(1024)
-        string = str(data)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen()
+conn, addr = s.accept()
+print(f"Server is started on {HOST}:{PORT}")
+while True:
+    data = conn.recv(1024)
+    string = str(data)
+    if string.strip():
         value = re.split('status=', string)[1]
         print(value)
         method = string.split()[0]
@@ -29,4 +30,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         conn.send(response_headers + f"Request Method: {method}"
                                      f"Request Source: ({HOST},{PORT})"
                                      f"Response Status: {status_value} {status_phrase}".encode("utf-8"))
-    # conn.close()
+        conn.close()
+        break
+    else:
+        print("invalid input, please check")
