15dcaa70a7b1384d1f8285157b81229236057803
tank34 <abgrund1@yandex.ru>
2022-06-12 19:35:46 +0300

    updated closure connection

--- src/server.py
+++ src/server.py
@@ -13,24 +13,21 @@ print(f"Server is started on {HOST}:{PORT}")
 while True:
     data = conn.recv(1024)
     string = str(data)
-    if string.strip():
-        value = re.split('status=', string)[1]
-        print(value)
-        method = string.split()[0]
-        for status in HTTPStatus:
-            if value == status:
-                status_value = status.value
-                status_phrase = status.phrase
-                break
-            else:
-                status_value = HTTPStatus.OK
-                status_phrase = HTTPStatus(HTTPStatus.OK).phrase
+    value = re.split('status=', string)[1]
+    print(value)
+    method = string.split()[0]
+    for status in HTTPStatus:
+        if value == status:
+            status_value = status.value
+            status_phrase = status.phrase
+            break
+        else:
+            status_value = HTTPStatus.OK
+            status_phrase = HTTPStatus(HTTPStatus.OK).phrase
 
-        response_headers = "Content-Type: text/html; charset=utf-8\n".encode("utf-8")
-        conn.send(response_headers + f"Request Method: {method}"
-                                     f"Request Source: ({HOST},{PORT})"
-                                     f"Response Status: {status_value} {status_phrase}".encode("utf-8"))
-        conn.close()
-        break
-    else:
-        print("invalid input, please check")
+    response_headers = "Content-Type: text/html; charset=utf-8\n".encode("utf-8")
+    conn.send(response_headers + f"Request Method: {method}"
+                                 f"Request Source: ({HOST},{PORT})"
+                                 f"Response Status: {status_value} {status_phrase}".encode("utf-8"))
+    conn.close()
+    break
