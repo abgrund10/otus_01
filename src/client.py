e653ad7a4571f292910d85a4a8fe3792aa76ee32
tank34 <abgrund1@yandex.ru>
2022-06-17 00:30:19 +0300

    try to get out from exception

--- src/client.py
+++ src/client.py
@@ -3,11 +3,15 @@
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+PORT = 65431  # The port used by the server
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall(b"GET http://127.0.0.1/status=404 NotFound")
-data = s.recv(1024)
-s.close()
-print("Received: " + str(data))
+while True:
+    message = input(" -> ")
+    s.sendall(str(b"{message}").format(message=message))
+    data = s.recv(1024).decode()
+    print("Received: " + str(data))
+    s.close()
+
+#  valid input: "GET http://127.0.0.1/status=404 NotFound"
--- src/server.py
+++ src/server.py
@@ -3,7 +3,7 @@ import re
 from http import HTTPStatus
 
 HOST = "127.0.0.1"
-PORT = 65432
+PORT = 65431
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
@@ -12,22 +12,25 @@ conn, addr = s.accept()
 print(f"Server is started on {HOST}:{PORT}")
 while True:
     data = conn.recv(1024)
-    string = str(data)
-    value = re.split('status=', string)[1]
-    print(value)
-    method = string.split()[0]
-    for status in HTTPStatus:
-        if value == status:
-            status_value = status.value
-            status_phrase = status.phrase
-            break
-        else:
-            status_value = HTTPStatus.OK
-            status_phrase = HTTPStatus(HTTPStatus.OK).phrase
+    string = data.decode('utf-8')
+    if string:
+        value = re.split('status=', string)[1]
+        print(value)
+        method = string.split()[0]
+        for status in HTTPStatus:
+            if value == status:
+                status_value = status.value
+                status_phrase = status.phrase
+                break
+            else:
+                status_value = HTTPStatus.OK
+                status_phrase = HTTPStatus(HTTPStatus.OK).phrase
 
-    response_headers = "Content-Type: text/html; charset=utf-8\n".encode("utf-8")
-    conn.send(response_headers + f"Request Method: {method}"
-                                 f"Request Source: ({HOST},{PORT})"
-                                 f"Response Status: {status_value} {status_phrase}".encode("utf-8"))
-    conn.close()
-    break
+        response_headers = "Content-Type: text/html; charset=utf-8\n".encode("utf-8")
+        conn.send(response_headers + f"Request Method: {method}"
+                                     f"Request Source: ({HOST},{PORT})"
+                                     f"Response Status: {status_value} {status_phrase}".encode("utf-8"))
+        conn.close()
+        break
+    if not string:
+        break
