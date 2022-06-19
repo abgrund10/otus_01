c186455b2afb506d0458c6a2b5f899a8ad052199
tank34 <abgrund1@yandex.ru>
2022-06-19 21:10:42 +0300

    update-1

--- src/client.py
+++ src/client.py
@@ -3,7 +3,7 @@
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65431  # The port used by the server
+PORT = 65432  # The port used by the server
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
