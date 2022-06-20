acbaffdd8787c4fad2d9707375b03a778cdb2d1e
tank34 <abgrund1@yandex.ru>
2022-06-20 23:17:48 +0300

    update-3

--- src/test_signup.py
+++ src/test_signup.py
@@ -11,7 +11,6 @@ n = random.randint(0, 220)
 @allure.step("SignIn option is present")
 def test_main_page(driver):
     try:
-      #  driver.get(url)
         driver.find_element_by_css_selector(LoginPage.Insta_link)
     except AssertionError:
         allure.attach("screenshot", attachment_type=allure.attachment_type.PNG)
@@ -23,7 +22,6 @@ def test_main_page(driver):
 @allure.step('Validation of functionality of return')
 def test_signin_back(driver):
     try:
-        driver.get(url)
         driver.find_element_by_xpath(LoginPage.SIGNIN).click()
         driver.find_element_by_css_selector(SignPage.BackButton).click()
         assert driver.find_element_by_css_selector(LoginPage.Insta_link)
@@ -37,7 +35,6 @@ def test_signin_back(driver):
 @allure.step('Validation of signin errors')
 def test_signin_errors(driver):
     try:
-        driver.get(url)
         driver.find_element_by_xpath(LoginPage.SIGNIN).click()
         driver.find_element_by_class_name(SignPage.SIGNIN).click()
         assert driver.find_element_by_xpath(SignPage.Error)
@@ -51,7 +48,6 @@ def test_signin_errors(driver):
 @allure.step("User registers with invalid email")
 def test_signin_invalid_email(driver):
     try:
-        driver.get(url)
         driver.find_element_by_xpath(LoginPage.SIGNIN).click()
         driver.find_element_by_id(SignPage.Email).click()
         driver.find_element_by_id(SignPage.Email).send_keys("@atp.com")
@@ -68,7 +64,6 @@ def test_signin_invalid_email(driver):
 @allure.step("User registers with invalid password")
 def test_signin_invalid_password(driver):
     try:
-        driver.get(url)
         driver.find_element_by_xpath(LoginPage.SIGNIN).click()
         driver.find_element_by_id(SignPage.Email).click()
         driver.find_element_by_id(SignPage.Email).send_keys("any@attp.com")
@@ -85,7 +80,6 @@ def test_signin_invalid_password(driver):
 @allure.step("User registers with invalid confirmation")
 def test_signin_invalid_confirmation(driver):
     try:
-        driver.get(url)
         driver.find_element_by_xpath(LoginPage.SIGNIN).click()
         driver.find_element_by_id(SignPage.Email).click()
         driver.find_element_by_id(SignPage.Email).send_keys("second2@attp.com")
@@ -103,7 +97,6 @@ def test_signin_invalid_confirmation(driver):
 def test_signin_successful(driver):
     random_email = "second" + f'{n}' + "@attp.com"
     try:
-        driver.get(url)
         driver.find_element_by_xpath(LoginPage.SIGNIN).click()
         driver.find_element_by_id(SignPage.Email).click()
         driver.find_element_by_id(SignPage.Email).send_keys(random_email)
@@ -121,7 +114,6 @@ def test_signin_successful(driver):
 def test_signin_location(driver):
     random_email2 = "second" + f'{n + 1}' + "@attp.com"
     try:
-        driver.get(url)
         driver.find_element_by_xpath(LoginPage.SIGNIN).click()
         driver.find_element_by_id(SignPage.Email).click()
         driver.find_element_by_id(SignPage.Email).send_keys(random_email2)
@@ -141,7 +133,6 @@ def test_signin_location(driver):
 @allure.step("User signups with existing creds")
 def test_existing_creds(driver):
     try:
-        driver.get(url)
         driver.find_element_by_xpath(LoginPage.SIGNIN).click()
         driver.find_element_by_id(SignPage.Email).click()
         driver.find_element_by_id(SignPage.Email).send_keys("second2@attp.com")
@@ -158,7 +149,6 @@ def test_existing_creds(driver):
 @allure.step("User observes filters")
 def test_requests(driver):
     try:
-        driver.get(url)
         driver.find_element_by_css_selector(LoginPage.Sign).click()
         driver.find_element_by_id(SignPage.Email).send_keys("second2@attp.com")
         driver.find_element_by_name(SignPage.Password).send_keys("1234567")
