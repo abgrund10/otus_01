376b345b9a1ccb3522477e6863f8a8804b85bbb5
tank34 <abgrund1@yandex.ru>
2022-06-22 00:27:14 +0300

    update-9

--- src/server.py
+++ src/server.py
@@ -1,9 +1,9 @@
 import socket
-import re
+from urllib.parse import urlparse, parse_qs
 from http import HTTPStatus
 
 HOST = "127.0.0.1"
-PORT = 65432
+PORT = 65431
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
@@ -13,24 +13,21 @@ conn, addr = s.accept()
 while True:
     data = conn.recv(1024)
     string = data.decode('utf-8')
-    if not string:
-        break
-    value = re.split('status=', string)[1]
-    method = string.split()[0]
-    #   status_code = re.findall('[0-9]+', value)
-    #   if status_code not in HTTPStatus:
-    #       status_value = 200
-    for status in HTTPStatus:
-        if value == status:
-            status_value = status.value
-            status_phrase = status.phrase
-            break
-        else:
-            status_value = HTTPStatus.OK
-            status_phrase = HTTPStatus(HTTPStatus.OK).phrase
+    # headers, body = string.split('\r\n\r\n')
+    # status_line, *headers = headers.split('\r\n')
+    method, url, protocol = string.split()
+    status = HTTPStatus(200)
+    query = urlparse(url).query
+    params = parse_qs(query)
+    if 'status' in params:
+        try:
+            code = int(params['status'][0])
+            status = HTTPStatus(code)
+        except ValueError as error:
+            pass
 
     response_headers = "Content-Type: text/html; charset=utf-8\n".encode("utf-8")
     conn.send(response_headers + f"Request Method: {method}\n"
                                  f"Request Source: ({HOST},{PORT})\n"
-                                 f"Response Status: {status_value} {status_phrase}".encode("utf-8"))
+                                 f"Response Status: {status.value} {status.phrase}".encode("utf-8"))
 conn.close()
